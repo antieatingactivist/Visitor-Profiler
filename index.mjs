ae50bf294291f0ba6c2e6fc9d25ccf39ba1aa034
antieatingactivist <antieatingactivist@gmail.com>
2022-10-18 22:03:32 -0700

    now writes data to file

--- index.mjs
+++ index.mjs
@@ -1,19 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import fs from 'fs';
+import data from './data.json' assert { type: "json" };
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.set('trust proxy', true)
+app.set('trust proxy', true);
 
-app.post("/", function(req, res) {
-    console.log(req)
+
+
+app.get("/", function(req, res) {
+    data.push(req.headers);
+    writeFile(JSON.stringify(data, null, 2));
     res.json(req.headers);  
 });
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
+
+function writeFile(data) {
+    fs.writeFileSync("./data.json", data, (err) => err ? console.log(err) : console.log("\x1b[1m\x1b[32m%s\x1b[0m", "\n -- Successfully added data to file"));
+}
