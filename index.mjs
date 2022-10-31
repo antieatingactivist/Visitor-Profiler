5800ea561ff1ed356c1688fa139137e6f3cc4f46
antieatingactivist <antieatingactivist@gmail.com>
2022-10-30 17:14:14 -0700

    added geolocation

--- index.mjs
+++ index.mjs
@@ -5,10 +5,12 @@ import fs from 'fs';
 import data from './data.json' assert { type: "json" };
 import path from 'path';
 import { fileURLToPath } from 'url';
+import axios from 'axios';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json());
@@ -17,14 +19,47 @@ app.set('trust proxy', true);
 
 
 
-app.get("/", function(req, res) {
+app.get("/", async function(req, res) {
+    const ip = req.headers["x-forwarded-for"];
 
-    if (req.headers["x-forwarded-for"].split('.')[0] !== "10") {
-        data.push(req.headers);
+    if (ip.split('.')[0] !== "10") {
+        const location = await axios.get(`https://api.ipdata.co/${ip}?api-key=${API_KEY}`);
+        const object = {...req.headers, location: {...location.data}}
+        data.push(object);
     }
 
     writeFile(JSON.stringify(data, null, 2));
     res.sendFile(__dirname +  "/index.html");  
+});
+
+app.get("/test", async function(req, res) {
+    const location = await axios.get(`https://api.ipdata.co/8.8.8.8?api-key=${API_KEY}`);
+
+    res.json(location.data);
+
+})
+
+app.get("/raw", function(req, res) {
+    res.json(data);  
+});
+
+app.get("/data", function(req, res) {
+    const responseData = [];
+    for (let visitor of data) {
+        let object = {};
+        object.ip = visitor['x-forwarded-for'];
+        object.time = new Date(visitor['x-start-time']).toLocaleString();
+        object.userAgent = visitor['user-agent'];
+ 
+
+        if (visitor['from']?.includes('bot') || object.userAgent?.includes('Expanse')) {
+            // responseData.push({"bot": " "});
+        }
+        
+        else responseData.push(object);
+    }
+
+    res.json(responseData);  
 });
 
 
