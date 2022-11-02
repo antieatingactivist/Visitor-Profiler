2c5a9cf70dec0e3496a63e0a9d78535ca7e4c3a8
antieatingactivist <antieatingactivist@gmail.com>
2022-11-01 18:43:09 -0700

    data is displayed in react app

--- src/index.mts
+++ src/index.mts
@@ -15,7 +15,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(cors());
-// app.use(express.static('views'));
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('trust proxy', true);
 
--- src/views/src/App.tsx
+++ src/views/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
+interface IVisitor {
+  ip: string,
+  time: string,
+  userAgent: string,
+  city: string,
+  region: string,
+  country: string,
+}
+
 function App() {
+  const [visitorData, setVisitorData] = useState<IVisitor[]>([]);
+  const getData = async () => {
+   const response = await fetch("/data");
+   const data = await response.json();
+   console.log(data);
+   setVisitorData(data);
+}
+  useEffect(() => {
+    getData();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {visitorData.map(visitor => (
+        <div>
+          <p>{visitor.ip}</p>
+          <p>{visitor.time}</p>
+          <p>{visitor.userAgent}</p>
+          <p>{visitor.city}</p>
+          <p>{visitor.region}</p>
+          <p>{visitor.country}</p>
+        </div>
+      ))}
     </div>
   );
 }
